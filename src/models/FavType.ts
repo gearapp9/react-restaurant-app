45ffeafe2c505e14bb1726d63dd93d29f5248dc3
Julia Hu <40742100+jhukosh@users.noreply.github.com>
2023-06-14 10:21:56 +0200

    Update FavType.ts

--- src/models/FavType.ts
+++ src/models/FavType.ts
@@ -1,3 +1,6 @@
+// I am not sure about that, is it an object ? 
+// I would not have put it there, as the isFav is really linked to the components logic
+// I am not saying it is wrong, on the contrary it is interesting :)
 export type favType = {
   favId: string;
   isFav: boolean;
